8c0048db888797e9b5b4935e74d1a949afdd9053
Jessica Overbey <jessica.overbey@gmail.com>
2015-03-18 17:07:04 -0400

    sass updates 2

--- gulpfile.js
+++ gulpfile.js
@@ -5,41 +5,40 @@ var gulp = require('gulp'),
     minifycss = require('gulp-minify-css'),
     newer = require('gulp-newer'),
     imagemin = require('gulp-imagemin'),
+    git = require('gulp-git'),
     livereload = require('gulp-livereload'),
     lr = require('tiny-lr'),
     server = lr();
- 
+
+var imgSrc = 'assets/images/originals/*';
+var imgDest = 'assets/images';
     
 gulp.task('styles', function(){
-  return gulp.src('scss/style.scss', {base: 'scss'})
-      .pipe(plumber())
-      .pipe(sass({ style: 'expanded' }))
+  return sass('scss/') 
+      .on('error', function (err) {
+          console.error('Error!', err.message);
+      })
       .pipe(gulp.dest(''))
       .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
       .pipe(minifycss())
-      .pipe(gulp.dest(''))
-      .pipe(livereload(server));
+      .pipe(gulp.dest(''));
 });
- 
- 
-var imgSrc = 'assets/images/originals/*';
-var imgDest = 'assets/images';
- 
+
 gulp.task('images', function() {
   return gulp.src(imgSrc, {base: 'assets/images/originals'})
         .pipe(newer(imgDest))
         .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
         .pipe(gulp.dest(imgDest));
 });
- 
+
 gulp.task('default', ['styles', 'images']);
- 
+
 gulp.task('watch', function() {
   // Listen on port 35729
   server.listen(35729, function (err) {
       if (err) {
-        return console.log(err)
-      };
+        return console.log(err);
+      }
   
       // Watch .scss files
       gulp.watch('scss/*.scss', ['styles']);
@@ -47,5 +46,14 @@ gulp.task('watch', function() {
       gulp.watch('assets/images/originals/**', ['images']);
   
     });
- 
+
 });
+gulp.task('init', function(){
+  git.init();
+});
+gulp.task('commit', function(){
+  return gulp.src('./*')
+  .pipe(git.add())
+  .pipe(git.commit('initial commit'));
+});
+gulp.task('setup',['styles','init','commit']);
